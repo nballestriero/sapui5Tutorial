539b4b6e279ca5845ce330692f2c695b7bf3df66
Emmanuel Golovanov <emmanuel.golovanov@outlook.com>
2018-03-08 17:25:14 +0100

    test add modules

--- app/controller/App.controller.js
+++ app/controller/App.controller.js
@@ -1,11 +1,11 @@
 sap.ui.define([
-   "sap/ui/core/mvc/Controller"
-], function (Controller) {
+   "sap/ui/core/mvc/Controller",
+   "sap/m/MessageToast"
+], function (Controller, MessageToast) {
    "use strict";
    return Controller.extend("app.controller.App", {
       onShowHello : function () {
-         // show a native JavaScript alert
-         alert("Hello World From Controller");
+         MessageToast.show("Hello World");
       }
    });
 });
