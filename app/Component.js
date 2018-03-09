32579a4fbf11cfd0eb8a3058569000c28b967a1c
Emmanuel Golovanov <emmanuel.golovanov@outlook.com>
2018-03-09 16:24:49 +0100

    Added Application's descriptor manifest.json

--- app/Component.js
+++ app/Component.js
@@ -1,18 +1,12 @@
 sap.ui.define([
    "sap/ui/core/UIComponent",
-   "sap/ui/model/json/JSONModel",
-   "sap/ui/model/resource/ResourceModel"
-], function (UIComponent, JSONModel, ResourceModel) {
+   "sap/ui/model/json/JSONModel"
+], function (UIComponent, JSONModel) {
    "use strict";
    return UIComponent.extend("app.Component", {
-            metadata : {
-		rootView: {
-			"viewName": "app.view.App",
-			"type": "XML",
-			"async": true,
-			"id": "app"
-		}
-	},
+      metadata : {
+            manifest: "json"
+      },
       init : function () {
          // call the init function of the parent
          UIComponent.prototype.init.apply(this, arguments);
@@ -24,12 +18,6 @@ sap.ui.define([
          };
          var oModel = new JSONModel(oData);
          this.setModel(oModel);
-
-         // set i18n model
-         var i18nModel = new ResourceModel({
-            bundleName : "app.i18n.i18n"
-         });
-         this.setModel(i18nModel, "i18n");
       }
    });
 });
