23fd20cbe67763313ade3927fe6ab8dd9ae4f1d0
Emmanuel Golovanov <emmanuel.golovanov@outlook.com>
2018-03-10 17:28:10 +0100

    added Formatter

--- app/controller/panelscontrollers/InvoiceList.controller.js
+++ app/controller/panelscontrollers/InvoiceList.controller.js
@@ -1,11 +1,12 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/ui/model/json/JSONModel"
-], function (Controller, JSONModel) {
+	"sap/ui/model/json/JSONModel",
+	"app/model/formatter"
+], function (Controller, JSONModel,Formatter) {
 	"use strict";
 
 	return Controller.extend("app.controller.panelscontrollers.InvoiceList", {
-
+	        formatter: Formatter,
 		onInit : function () {
 			var oViewModel = new JSONModel({
 				currency: "EUR"
