205d67ef18e8a77f821fba7c2b0d2dd3f05bd53d
Emmanuel Golovanov <emmanuel.golovanov@outlook.com>
2018-03-11 09:58:48 +0100

    Added filter on one table's field

--- app/controller/panelscontrollers/InvoiceList.controller.js
+++ app/controller/panelscontrollers/InvoiceList.controller.js
@@ -1,8 +1,10 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/ui/model/json/JSONModel",
-	"app/model/formatter"
-], function (Controller, JSONModel,Formatter) {
+	"app/model/formatter",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
+], function (Controller, JSONModel,Formatter,Filter, FilterOperator) {
 	"use strict";
 
 	return Controller.extend("app.controller.panelscontrollers.InvoiceList", {
@@ -12,6 +14,20 @@ sap.ui.define([
 				currency: "EUR"
 			});
 			this.getView().setModel(oViewModel, "view");
+		},
+		onFilterInvoices : function (oEvent) {
+
+			// build filter array
+			var aFilter = [];
+			var sQuery = oEvent.getParameter("query");
+			if (sQuery) {
+				aFilter.push(new Filter("ProductName", FilterOperator.Contains, sQuery));
+			}
+
+			// filter binding
+			var oList = this.byId("invoiceList");
+			var oBinding = oList.getBinding("items");
+			oBinding.filter(aFilter);
 		}
 
 	});
